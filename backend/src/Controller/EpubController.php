cc33c245011fa348cb306224b5cce90d893a05b8
Valentyn Saik <svbackend22@gmail.com>
2021-02-28 09:30:09 +0200

    Stash progress

--- backend/src/Controller/EpubController.php
+++ backend/src/Controller/EpubController.php
@@ -24,13 +24,37 @@ class EpubController extends BaseApiController
         foreach ($toc as $tocItem) {
             $refItemHref = basename($tocItem['file_name']);
             $chapterContent = $parse->getChapterByHref($refItemHref);
-            dump($chapterContent);
             $chapterCards = $this->getCardsByContent($chapterContent);
-            dd($chapterCards);
+            //dd($chapterCards);
             $cards += $chapterCards;
         }
 
-        dd($cards);
+        $removeCardsIdx = [];
+        $len = count($cards);
+        for ($i = 0, $k = 1; $i < $len && $k < $len; $k++) {
+            $currentCardSize = mb_strlen(strip_tags($cards[$i]));
+            dump(strip_tags($cards[$i]), $currentCardSize, $i, $k);
+            if ($currentCardSize < 140) {
+                $cards[$i] .= $cards[$k];
+                $removeCardsIdx[] = $k;
+                if ($currentCardSize > 140) {
+                    $i = ++$k;
+                    continue;
+                }
+            } elseif ($i+1 === $k) {
+                $i++;
+            } else {
+                $i = ++$k;
+            }
+        }
+        //$cards[--$i] .= $cards[$len - 1];
+        //$cards[] =
+
+        foreach ($removeCardsIdx as $idx) {
+            unset($cards[$idx]);
+        }
+
+        dd($cards, $removeCardsIdx);
 
         return $this->ack();
     }
@@ -60,13 +84,10 @@ class EpubController extends BaseApiController
         foreach ($children as $child) {
             if (mb_strlen($child->text()) <= 200) {
                 $cards = array_merge($cards, [$child->html()]);
-                dump('ADDED CARD: ', $child->html());
             } else {
                 [$currentElPrefix, $currentElPostfix] = $this->extractHtmlElementPrefixAndPostfix($child->outerHtml());
 
                 if (trim($child->innerHtml())) {
-                    dump('RECURSIVE CALL: ', $child->innerHtml());
-
                     $cards = array_merge(
                         $cards,
                         $this->addHtmlTagsToCards($this->getCardsByContent($child->innerHtml()), $currentElPrefix, $currentElPostfix)
@@ -99,6 +120,11 @@ class EpubController extends BaseApiController
         $outerHtml = trim($outerHtml);
         $leftArrowPosition = mb_strpos($outerHtml, '<');
         $rightArrowPosition = mb_strpos($outerHtml, '>');
+
+        if ($rightArrowPosition === false || $leftArrowPosition === false) {
+            return ['', ''];
+        }
+
         $el = mb_substr($outerHtml, $leftArrowPosition, $rightArrowPosition + 1);
 
         $tagNameEndPosition = mb_strpos($el, ' ');
